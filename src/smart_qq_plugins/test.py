fd8b2e9ede449f5c0156ab915130e648030a29e7
cj19950911 <455077686@qq.com>
2017-07-27 16:24:47 +0800

    Update test.py

--- src/smart_qq_plugins/test.py
+++ src/smart_qq_plugins/test.py
@@ -1,25 +1,23 @@
+import thread  
 from random import randint
 
 from smart_qq_bot.messages import GroupMsg, PrivateMsg
 from smart_qq_bot.signals import on_all_message, on_bot_inited
 from smart_qq_bot.logger import logger
 
+def tick():
+       print('Tick! The time is:a')
+        
+def loop0():
+      scheduler = BlockingScheduler()
+     scheduler.add_job(tick,'cron', second='*/20', hour='*')    
+     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown() 
+
 
 @on_bot_inited("PluginManager")
 def manager_init(bot):
     logger.info("Plugin Manager is available now:)")
-
-@on_all_message(name="SamplePlugin")
-def sample_plugin(msg, bot):
-    """
-    :type bot: smart_qq_bot.bot.QQBot
-    :type msg: smart_qq_bot.messages.GroupMsg
-    """
-    msg_id = randint(1, 10000)
-    
-    # 发送一条群消息
-    if isinstance(msg, GroupMsg):
-        bot.send_group_msg("msg", msg.from_uin, msg_id)
-    # 发送一条私聊消息
-    elif isinstance(msg, PrivateMsg):
-        bot.send_friend_msg("msg", msg.from_uin, msg_id)
+　　thread.start_new_thread(loop0, ())  
